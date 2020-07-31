5693fe9845215bb44d18950721df23c183066554
al6ert <albertperezrabella@hotmail.com>
2020-07-31 10:12:34 +0200

    Testing with Slack

--- functions/landbot.js
+++ functions/landbot.js
@@ -49,3 +49,43 @@ exports.sendLocation = functions.https.onCall(async (data, _context) => {
 		throw new Error(error);
 	}
 });
+
+exports.createSlackConversation = functions.https.onCall(async (data, _context) => {
+
+	if (!data.token) {
+		var request = require('request-promise');
+		console.log(data);
+		var options = {
+			'method': 'GET',
+			'url':'https://slack.com/oauth/authorize'
+			
+		}
+
+	}
+	
+	/*
+	var request = require('request-promise');
+	console.log(data);
+	console.log(`https://api.landbot.io/v1/channels/${data.channelId}/message_hooks/`);
+	var options = {
+		'method': 'POST',
+		'url': `https://api.landbot.io/v1/channels/${data.cannelId}/message_hooks/`,
+		'headers': {
+		  'Authorization': functions.config().env[data.account].landbot.token,//data.token,
+		  'Content-Type': ['application/json', 'application/json']
+		},
+		body: JSON.stringify({
+			"url": "https://5efa5169abdfa1af788455afc5b00fd8.m.pipedream.net",
+			"name": "from_landbot_to_slack_hook",
+			"token": ""
+		})
+	  };	
+	  try {
+		  const body = await request(options);		
+		  return JSON.parse(body);
+	  }
+	  catch (error) {
+		  throw new Error(error);
+	  }*/
+
+});
